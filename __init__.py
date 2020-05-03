0c07f353459fd3c0f14a4e9a942726e91ff926e2
Arthur Milchior <Arthur@Milchior.fr>
2020-05-03 19:47:04 +0200

    remove keepModelInAddCards and an indirection

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@
 # Original code from Anki, copyright Damien Elmes <anki@ichi2.net>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 # Add-on number 424778276 https://ankiweb.net/shared/info/424778276
-from . import keepModelInAddCards
+from . import addCards
--- keepModelInAddCards.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Github: https://github.com/Arthur-Milchior/anki-keep-model-in-add-cards
-# Original code from Anki, copyright Damien Elmes <anki@ichi2.net>
-# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-# Add-on number 424778276 https://ankiweb.net/shared/info/424778276
-from . import addCards
